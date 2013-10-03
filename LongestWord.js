f63c68aa22f2cd4e9a94b1e3ee93f4be67612c03
Adam Richman <ar@adamrichman.com>
2013-10-03 15:10:18 -0400

    fixed LongestWord.js, now works for coderbyte test cases

--- LongestWord.js
+++ LongestWord.js
@@ -3,28 +3,36 @@ and return the largest word in the string. If there are two or more words
 that are the same length, return the first word from the string with that 
 length. Ignore punctuation and assume sen will not be empty.  */
 
-// THIS ONE IS BROKEN
-
 function LongestWord(sen){ 
-  var senArray = sen.replace(/[^\w\s]|_/g, "").split(' ');
+  var senArray = sen.split(' ');
+  console.log('senArray[0]: ' + senArray[0]);
+  var longestWord = { 'longWord': senArray[0] };
+  console.log(longestWord.longWord);
+  console.log(longestWord.longWord.length);
+  console.log(senArray.length);
+  var testWord = { 'longWord': longestWord.longWord } ;
+  console.log('testWord: ' + testWord.longWord);
   console.log(senArray);
-  var longestWord = senArray[0];
-  longestWord.toString();
-  for (i = 0; i <= senArray.length; i++){
-    var testWord = senArray[i];
-    if (longestWord.length < testWord.length ){
-        longestWord = testWord;
-//        return longestWord;
+
+ for (i = 1; i < senArray.length; i++){
+   console.log(senArray);
+   testWord.longWord = senArray[i];
+   console.log('testword: ' + testWord.longWord);
+    if (longestWord.longWord.length < testWord.longWord.length ){
+        longestWord.longWord = testWord.longWord;
+        return longestWord.longWord;
     } 
     // else {
-    //   return longestWord;
+      
+    //   return longestWord.longWord;
     // } 
   }
   
   
   
-  return longestWord; 
+   return longestWord.longWord; 
          
 }
-
-LongestWord('this is some sort of sentence');
+LongestWord('a b c dee');
+//LongestWord('coderbyte');
+//LongestWord('this is some sort of sentence');
