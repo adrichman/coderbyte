41a981c79b06b9ea3444b2442a3c3b432e70c312
Adam Richman <ar@adamrichman.com>
2013-10-12 13:24:05 -0400

    added BracketMatcher.js, works with test cases

--- BracketMatcher.js
+++ BracketMatcher.js
@@ -4,4 +4,34 @@ passed and return 1 if the brackets are correctly matched and each one is accoun
 return 0. For example: if str is "(hello (world))", then the output should be 1, but if str is 
 "((hello (world))" the the output should be 0 because the brackets do not correctly match up. 
 Only "(" and ")" will be used as brackets. If str contains no brackets return 1. 
+
+Input = "(coder)(byte))"Output = 0
+Input = "(c(oder)) b(yte)"Output = 1
 */
+function BracketMatcher(str){
+	bracket1Arr = [];
+	bracket2Arr = [];
+	for (i=0; i<str.length;i++){
+
+		if(str.charAt(i) == "("){
+			bracket1Arr.push(str.charAt(1));
+		} else if (str.charAt(i) == ")"){
+			bracket2Arr.push(str.charAt(1));
+		}
+		if(bracket2Arr.length > bracket1Arr.length){
+			return 0;
+		}
+
+	}
+	if (bracket1Arr.length == bracket2Arr.length){
+		return 1;
+	} else {
+		return 0;
+	}
+
+}
+
+//BracketMatcher("(coder)(byte))");
+//BracketMatcher("(c(oder)) b(yte)");
+//BracketMatcher("(c(o(())((der)) b(y))te)");
+BracketMatcher("the color re(d))()(()");
